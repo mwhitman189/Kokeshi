5bb0c32842e9a5b151352b6132727092744742b7
mWhitman189 <mwhitman189@gmail.com>
2019-01-02 00:54:51 +0900

    Change default port to 5432

--- app.py
+++ app.py
@@ -170,5 +170,5 @@ def showContactPage():
 ### Initialize App ###
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 8000.
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 5432))
     app.run(host='0.0.0.0', port=port)
