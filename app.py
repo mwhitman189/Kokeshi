35d020e8efe24aa27600d50fdfd023538d0d5c96
mWhitman189 <mwhitman189@gmail.com>
2019-01-03 23:31:19 +0900

    Remove DATABASE_URL print statement

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ csrf = SeaSurf(app)
 app.config.from_pyfile('config_default.cfg')
 app.config.from_envvar('KOKESHI_SETTINGS')
 
-print(app.config['SQLALCHEMY_DATABASE_URI'])
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
 db = SQLAlchemy(app)
