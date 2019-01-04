2f337c2a7e3f00f202c3ed9281e49293d02802a2
mWhitman189 <mwhitman189@gmail.com>
2019-01-04 14:22:17 +0900

    Change procfile to app:app

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config.from_pyfile('config_default.cfg')
 app.config.from_envvar('KOKESHI_SETTINGS')
 
 
-#engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
 db = SQLAlchemy(app)
 
