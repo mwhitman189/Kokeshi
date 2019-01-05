7dd9c0b2c5a53ac34bb44dab6d5b6927854f3630
mWhitman189 <mwhitman189@gmail.com>
2019-01-06 00:37:07 +0900

    Rerun migrations

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from models import Customer, User, Order
-from app import create_app
+from appInit import create_app
 
 app = Flask(__name__)
 
