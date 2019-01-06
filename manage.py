e0a4b2f4ff8a43f77410214a6ddd084eaceef3cc
mWhitman189 <mwhitman189@gmail.com>
2019-01-06 13:37:45 +0900

    Add db import to manage.py

--- manage.py
+++ manage.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from models import Customer, User, Order
-from appInit import create_app
+from models import Customer, User, CustomerOrder
+from appInit import create_app, app, db
 
 app = Flask(__name__)
 
