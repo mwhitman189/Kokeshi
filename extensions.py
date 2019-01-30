b2e7acd08372ddd145a8ff8ed9b6f292715196f9
mWhitman189 <mwhitman189@gmail.com>
2019-01-30 20:08:16 +0900

    Add flask-mail import and init

--- extensions.py
+++ extensions.py
@@ -5,6 +5,7 @@ from flask_wtf.csrf import CSRFProtect
 from flask_marshmallow import Marshmallow
 from flask_login import LoginManager
 from flask_heroku import Heroku
+from flask_mail import Mail
 
 
 db = SQLAlchemy()
@@ -13,3 +14,4 @@ csrf = CSRFProtect()
 ma = Marshmallow()
 login_manager = LoginManager()
 heroku = Heroku()
+mail = Mail()
