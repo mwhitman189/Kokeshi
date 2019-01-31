ff4354db83b9f1eccd1b3ab722649714925f0055
mWhitman189 <mwhitman189@gmail.com>
2019-01-31 22:27:38 +0900

    Change response header max duration to 0 to test session

--- appInit.py
+++ appInit.py
@@ -68,6 +68,14 @@ def create_app():
     #################
     # App functions #
     #################
+    @app.after_request
+    def add_header(response):
+        """
+        Add headers to both force latest IE rendering engine or Chrome Frame,
+        and also to cache the rendered page for 10 minutes.
+        """
+        response.headers['Cache-Control'] = 'public, max-age=0'
+        return response
 
     #######################
     # User administration #
