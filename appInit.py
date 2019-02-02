c230e2f5923c5f70144b68b0345d2fcaf48e340c
mWhitman189 <mwhitman189@gmail.com>
2019-02-03 00:41:16 +0900

    Remove faulty fix for login

--- appInit.py
+++ appInit.py
@@ -500,8 +500,4 @@ def create_app():
     heroku.init_app(app)
     mail.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.get(user_id)
-
     return app
